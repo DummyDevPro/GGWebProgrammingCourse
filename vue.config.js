6b2e9193333627e00ac41f397306a00391c19440
DummyDevPro <htun@osf.ac.jp>
2024-01-29 23:04:43 +0900

    remove git ignore of dist & build

--- vue.config.js
+++ vue.config.js
@@ -4,23 +4,8 @@ module.exports = defineConfig({
     ? '/'
     : './',
   transpileDependencies: true,
-  outputDir: 'docs',
+  outputDir: 'dist',
   chainWebpack: (config) => {
-    //   config.resolve.alias.set('vue', '@vue/compat')
-
-    //   config.module
-    //     .rule('vue')
-    //     .use('vue-loader')
-    //     .tap((options) => {
-    //       return {
-    //         ...options,
-    //         compilerOptions: {
-    //           compatConfig: {
-    //             MODE: 2
-    //           }
-    //         }
-    //       }
-    //     })
 
     config.plugin('define').tap((definitions) => {
       Object.assign(definitions[0], {
@@ -32,3 +17,39 @@ module.exports = defineConfig({
     })
   }
 })
+
+//
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/'
+//     : './',
+//   transpileDependencies: true,
+//   outputDir: 'docs',
+//   chainWebpack: (config) => {
+//     //   config.resolve.alias.set('vue', '@vue/compat')
+
+//     //   config.module
+//     //     .rule('vue')
+//     //     .use('vue-loader')
+//     //     .tap((options) => {
+//     //       return {
+//     //         ...options,
+//     //         compilerOptions: {
+//     //           compatConfig: {
+//     //             MODE: 2
+//     //           }
+//     //         }
+//     //       }
+//     //     })
+
+//     config.plugin('define').tap((definitions) => {
+//       Object.assign(definitions[0], {
+//         __VUE_OPTIONS_API__: 'true',
+//         __VUE_PROD_DEVTOOLS__: 'false',
+//         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+//       })
+//       return definitions
+//     })
+//   }
+// })
